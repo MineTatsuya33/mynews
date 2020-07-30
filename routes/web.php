f1937e7c7d3814f98192d70761b8a2dffcb885ea
Tatsuya <tm33keiogk@gmail.com>
2020-07-30 00:12:12 +0000

    re

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,11 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
 });
 
 Route::get('XXX',
 'Admin\AAAController@bbb');
 
-Route::get('admin/profile/create',
-'Admin\ProfileController@add');
 
-Route::get('admin/profile/edit',
-'Admin\ProfileController@edit');
